a75e1b453a51dd2590ed30495dc64f7969bf421e
Giacomo Cerquone <cerquone@gmail.com>
2019-02-16 16:56:43 +0100

    last fix

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ const ghApiUrl = 'https://github-trending-api.now.sh/repositories?language=javas
 const CNApiUrl = 'https://api.chucknorris.io/jokes/random';
 
 app.innerHTML = `
-    <div style="text-align: center;">
-      <h1 style="margin-bottom: 2em;">Le Repo più famose</h1>
-      <p id="CNSentence">A roundhouse kick!!!</p>
-      <div style="display: flex; justify-content: center">
-        <button id="get-repo">Prendi le Repositories</button>
-      </div>
-      <div id="repo-boxes" style="display: flex; justify-content: space-between; flex-wrap: wrap;"></div>
+  <div style="text-align: center;">
+    <h1 style="margin-bottom: 2em;">Le Repo più famose</h1>
+    <p id="CNSentence">A roundhouse kick!!!</p>
+    <div style="display: flex; justify-content: center">
+      <button id="get-repo">Prendi le Repositories</button>
     </div>
+    <div id="repo-boxes" style="display: flex; justify-content: space-between; flex-wrap: wrap;"></div>
+  </div>
 `;
 
+const getRepo = document.getElementById('get-repo');
+const repoBoxes = document.getElementById('repo-boxes');
+const chuckNorrisBox = document.getElementById('CNSentence');
+
 function createBox({
-  author, url, name, stars, forks, language,
+  url, name, stars, author, forks, language,
 }) {
   const box = document.createElement('div');
   box.className = 'repo-box';
@@ -26,13 +30,8 @@ function createBox({
     <p class="author">Autore: ${author}</p>
     <button class="remove">x</button>
   `;
-
   return box;
 }
-
-const getRepo = document.getElementById('get-repo');
-const repoBoxes = document.getElementById('repo-boxes');
-const chuckNorrisBox = document.getElementById('CNSentence');
 
 repoBoxes.addEventListener('click', (e) => {
   if (e.target && e.target.className === 'remove') {
@@ -40,20 +39,43 @@ repoBoxes.addEventListener('click', (e) => {
   }
 }, false);
 
+
 getRepo.addEventListener('click', async () => {
   try {
-    // const repos = await (await fetch(ghApiUrl)).json();
+    // const res = await fetch(ghApiUrl);
+    // const repos = await res.json();
+
+    // oppure: const repos = await (await fetch(ghApiUrl)).json();
+
+    // const myProms = [
+    //   fetch(ghApiUrl).then(response => response.json()),
+    //   fetch(CNApiUrl).then(response => response.json()),
+    // ];
+    //
+    // const [repos, chuckNorris] = await Promise.all(myProms);
+
+    // oppure
+
+    // SBAGLIATO
 
     const myProms = [
-      await (await fetch(ghApiUrl)).json(),
-      await (await fetch(CNApiUrl)).json(),
+      fetch(ghApiUrl),
+      fetch(CNApiUrl),
     ];
 
-    const [repos, chuckNorris] = await Promise.all(myProms);
 
+    const [res1, res2] = await Promise.all(myProms);
+
+    const myProms2 = [
+      res1.json(),
+      res2.json(),
+    ];
+
+    const [repos, chuckNorris] = await Promise.all(myProms2);
+
+    // Reset Boxes Content on new Fetch
     repoBoxes.innerHTML = '';
     chuckNorrisBox.innerHTML = `${chuckNorris.value}`;
-
 
     repos.forEach((repo) => {
       repoBoxes.appendChild(createBox(repo));
